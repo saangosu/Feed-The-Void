73928c1d36f0890fed161fda35c553fac7539ffa
saangosu <saangosu@gmail.com>
2024-05-18 13:50:54 -0500

    blink n main

--- entities/void_creature.gd
+++ entities/void_creature.gd
@@ -1,8 +1,26 @@
 extends Node2D
 
 @export var minSize := 1.0        # Will be the original scale of the object
-@export var maxSize := 10.0       # Will be the max scale of the object
+@export var maxSize := 9.0       # Will be the max scale of the object
+var sizePercentage := 1.0
 
 func _ready():
-	minSize = scale.x
+	minSize = scale.y
 	maxSize = minSize * maxSize
+	sizePercentage = scale.y * 100 / maxSize
+
+func _on_blink_timer_timeout():
+	$EyeL.animation = "Blink"
+	$EyeL.play()
+
+func _physics_process(delta):
+	print(scale.y < maxSize)
+	if scale.y < maxSize:
+		scale *= Vector2(1.5, 1.5)
+		sizePercentage = scale.y * 100 / maxSize
+
+
+func _on_eye_l_animation_finished():
+	$EyeL.animation = "Idle"
+	$EyeL.speed_scale = 1.0
+	$EyeL.play()
